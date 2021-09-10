77f9c8bf4082afaed0eb280982563bb3ccc13d77
srmbshishir <srmb.shishir@gmail.com>
2021-09-10 15:21:29 +0600

    high chart added

---
+++ Task2-Chart/HighChart/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HighChart.Models;
+
+namespace HighChart.Controllers
+{
+    public class HomeController : Controller
+    {
+        FinalTechnicalTestEntities context = new FinalTechnicalTestEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public JsonResult GetData()
+        {
+            return Json(context.Stocks.ToList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
